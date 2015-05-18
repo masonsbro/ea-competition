3b47997fc723c211f59f26fd236afbc2cfe261dd
Ethan Arnold <masonsbro456@gmail.com>
2015-05-17 19:46:48 -0500

    material design because trendy

--- public/dist/application.js
+++ public/dist/application.js
@@ -70,17 +70,7 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
 	function($scope, Authentication, Menus) {
 		$scope.authentication = Authentication;
-		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
-
-		$scope.toggleCollapsibleMenu = function() {
-			$scope.isCollapsed = !$scope.isCollapsed;
-		};
-
-		// Collapsing the menu after navigation
-		$scope.$on('$stateChangeSuccess', function() {
-			$scope.isCollapsed = false;
-		});
 	}
 ]);
 'use strict';
